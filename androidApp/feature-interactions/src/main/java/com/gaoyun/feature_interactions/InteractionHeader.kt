c7a94e6e01e32fbc7b67d5b58a8dc07f2b5ebf9c
artem.zasypalov <gaoyundexinmen@gmail.com>
2022-11-26 16:20:06 +0100

    Add draft reminder screen content

---
+++ androidApp/feature-interactions/src/main/java/com/gaoyun/feature_interactions/InteractionHeader.kt
@@ -0,0 +1,110 @@
+package com.gaoyun.feature_interactions
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.gaoyun.common.ui.Spacer
+import com.gaoyun.roar.model.domain.Pet
+import com.gaoyun.roar.model.domain.interactions.InteractionGroup
+import com.gaoyun.roar.model.domain.interactions.InteractionWithReminders
+
+@Composable
+internal fun InteractionHeader(
+    pet: Pet,
+    interaction: InteractionWithReminders,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(horizontal = 8.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = interaction.name,
+                style = MaterialTheme.typography.displayMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.weight(0.8f)
+            )
+
+            Spacer(8.dp)
+
+            Surface(
+                tonalElevation = 8.dp,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(0.2f),
+                shape = CircleShape,
+            ) {
+                Image(
+                    painter = painterResource(id = com.gaoyun.common.R.drawable.ic_cat),
+                    contentDescription = pet.name,
+                    modifier = Modifier
+                        .height(72.dp)
+                        .padding(all = 12.dp)
+                )
+            }
+        }
+
+        Spacer(size = 32.dp)
+
+        Card(
+            elevation = CardDefaults.elevatedCardElevation(0.dp),
+            shape = MaterialTheme.shapes.large,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+            ) {
+                val groupIcon = when (interaction.group) {
+                    InteractionGroup.HEALTH -> Icons.Filled.MedicalServices
+                    InteractionGroup.CARE -> Icons.Filled.Spa
+                    InteractionGroup.ROUTINE -> Icons.Filled.Pets
+                }
+                TextWithIconBulletPoint(icon = groupIcon, interaction.group.toString())
+
+                interaction.repeatConfig?.let {
+                    TextWithIconBulletPoint(icon = Icons.Filled.Repeat, it.toString())
+                } ?: TextWithIconBulletPoint(icon = Icons.Filled.Repeat, "Doesn't repeat")
+
+                if (interaction.notes.isNotEmpty()) {
+                    TextWithIconBulletPoint(icon = Icons.Filled.Notes, interaction.notes)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TextWithIconBulletPoint(icon: ImageVector, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 4.dp)
+    ) {
+        Icon(
+            icon,
+            text,
+            tint = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(4.dp),
+        )
+        Spacer(size = 12.dp)
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
--- androidApp/feature-interactions/src/main/java/com/gaoyun/feature_interactions/InteractionScreen.kt
+++ androidApp/feature-interactions/src/main/java/com/gaoyun/feature_interactions/InteractionScreen.kt
@@ -1,17 +1,23 @@
 package com.gaoyun.feature_interactions
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavHostController
 import com.gaoyun.common.OnLifecycleEvent
+import com.gaoyun.common.ui.LabelledCheckBox
 import com.gaoyun.common.ui.Loader
 import com.gaoyun.common.ui.RoarExtendedFloatingActionButton
 import com.gaoyun.common.ui.SurfaceScaffold
@@ -80,10 +86,76 @@ fun InteractionScreen(
     ) {
         Box {
             state.interaction?.let { interaction ->
-            }
+                state.pet?.let { pet ->
+                    LazyColumn(modifier = Modifier.padding(horizontal = 8.dp)) {
+                        item {
+                            InteractionHeader(
+                                pet = pet,
+                                interaction = interaction,
+                                modifier = Modifier.padding(top = 32.dp)
+                            )
+                        }
 
-            Loader(isLoading = state.isLoading)
+                        item {
+                            Text(
+                                text = "Reminders",
+                                style = MaterialTheme.typography.headlineMedium,
+                                color = MaterialTheme.colorScheme.onSurface,
+                                modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)
+                            )
+                        }
+
+                        item {
+                            Surface(
+                                tonalElevation = 4.dp,
+                                shape = MaterialTheme.shapes.large,
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp, vertical = 8.dp)
+                                    .fillMaxWidth(),
+                            ) {
+                                Column {
+                                    interaction.reminders.map { reminder ->
+                                        LabelledCheckBox(
+                                            checked = !reminder.isCompleted,
+                                            label = "${reminder.dateTime.date} at 09:00",
+                                            modifier = Modifier.fillMaxWidth(),
+                                            verticalPadding = 16.dp,
+                                            horizontalPadding = 20.dp,
+                                            spacerSize = 14.dp,
+                                            onCheckedChange = {}
+                                        )
+                                    }
+                                    interaction.reminders.map { reminder ->
+                                        LabelledCheckBox(
+                                            checked = !reminder.isCompleted,
+                                            label = "${reminder.dateTime.date} at 09:00",
+                                            modifier = Modifier.fillMaxWidth(),
+                                            verticalPadding = 16.dp,
+                                            horizontalPadding = 20.dp,
+                                            spacerSize = 14.dp,
+                                            onCheckedChange = {}
+                                        )
+                                    }
+                                    interaction.reminders.map { reminder ->
+                                        LabelledCheckBox(
+                                            checked = !reminder.isCompleted,
+                                            label = "${reminder.dateTime.date} at 09:00",
+                                            modifier = Modifier.fillMaxWidth(),
+                                            verticalPadding = 16.dp,
+                                            horizontalPadding = 20.dp,
+                                            spacerSize = 14.dp,
+                                            onCheckedChange = {}
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+
+        Loader(isLoading = state.isLoading)
     }
 }
 
