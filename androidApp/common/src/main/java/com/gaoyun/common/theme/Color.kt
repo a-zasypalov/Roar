0215b9803ebb84764e895333ae97e5bcdfbbda8d
artem.zasypalov <gaoyundexinmen@gmail.com>
2022-11-27 13:56:04 +0100

    Static theme colors definition

--- androidApp/common/src/main/java/com/gaoyun/common/theme/Color.kt
+++ androidApp/common/src/main/java/com/gaoyun/common/theme/Color.kt
@@ -2,14 +2,59 @@ package com.gaoyun.common.theme
 
 import androidx.compose.ui.graphics.Color
 
-val RoarLightGray = Color(0xFFEBEBEB)
-val RoarDarkGray = Color(0xFF8A8A8A)
+//Light colours
 val RoarOrange = Color(0xFFFF9C36)
+val RoarPrimary = Color(0xFFDEA33E)
+val RoarOnPrimary = Color(0xFFFFFFFF)
+val RoarPrimaryContainer = Color(0xFFFFDEAA)
+val RoarOnPrimaryContainer = Color(0xFF271900)
 
-val RoarBlack = Color(0xFF20201F)
+val RoarTertiary = Color(0xFF4E662A)
+val RoarOnTertiary = Color(0xFFFFFFFF)
+val RoarTertiaryContainer = Color(0xFFCFEDA1)
+val RoarOnTertiaryContainer = Color(0xFF111F00)
 
-val RoarWhite = Color(0xFFF4F9FD)
-val RoarWhiteSemitransparent = Color(0x88F4F9FD)
+val RoarBackground = Color(0xFFFAFAE6)
+val RoarOnBackground = Color(0xFF191D11)
 
-val DarkBackground = Color(0xFF000000)
-val DarkSurface = Color(0xFF121212)
+val RoarError = Color(0xFFB3261E)
+val RoarOnError = Color(0xFFFFFFFF)
+val RoarErrorContainer = Color(0xFFF9DEDC)
+val RoarOnErrorContainer = Color(0xFF410E0B)
+
+val RoarOutline = Color(0xFF707A5F)
+
+val RoarSurface = Color(0xFFF3F7E1)
+val RoarOnSurface = Color(0xFF191D11)
+val RoarSurfaceVariant = Color(0xFFDCE7C7)
+val RoarOnSurfaceVariant = Color(0xFF414A33)
+val RoarSurfaceInverse = Color(0xFF2D3225)
+val RoarOnSurfaceInverse = Color(0xFFEEF3DF)
+
+//DarkColours
+val RoarPrimaryDark = Color(0xFFF9BC49)
+val RoarOnPrimaryDark = Color(0xFF422C00)
+val RoarPrimaryContainerDark = Color(0xFF5F4100)
+val RoarOnPrimaryContainerDark = Color(0xFFFFDEAA)
+
+val RoarTertiaryDark = Color(0xFFB3D088)
+val RoarOnTertiaryDark = Color(0xFF213600)
+val RoarTertiaryContainerDark = Color(0xFF374E14)
+val RoarOnTertiaryContainerDark = Color(0xFFCFEDA1)
+
+val RoarBackgroundDark = Color(0xFF151515)
+val RoarOnBackgroundDark = Color(0xFFE0E5D1)
+
+val RoarErrorDark = Color(0xFFF2B8D5)
+val RoarOnErrorDark = Color(0xFF601410)
+val RoarErrorContainerDark = Color(0xFF8C1D18)
+val RoarOnErrorContainerDark = Color(0xFFF2B8B5)
+
+val RoarOutlineDark = Color(0xFF8B9478)
+
+val RoarSurfaceDark = Color(0xFF191D11)
+val RoarOnSurfaceDark = Color(0xFFE0E5D1)
+val RoarSurfaceVariantDark = Color(0xFF414A33)
+val RoarOnSurfaceVariantDark = Color(0xFFC0CAAC)
+val RoarSurfaceInverseDark = Color(0xFFE0E5D1)
+val RoarOnSurfaceInverseDark = Color(0xFF2D3225)
