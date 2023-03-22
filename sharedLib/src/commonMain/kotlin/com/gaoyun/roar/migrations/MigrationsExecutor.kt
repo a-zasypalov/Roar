1550c7c339e807e4d7f07b8097e605c79bd6452b
artem.zasypalov <gaoyundexinmen@gmail.com>
2023-03-22 18:56:04 +0100

    Migration work

--- sharedLib/src/commonMain/kotlin/com/gaoyun/roar/migrations/MigrationsExecutor.kt
+++ sharedLib/src/commonMain/kotlin/com/gaoyun/roar/migrations/MigrationsExecutor.kt
@@ -1,7 +1,9 @@
 package com.gaoyun.roar.migrations
 
 import com.gaoyun.roar.model.entity.RoarDatabase
+import com.squareup.sqldelight.db.AfterVersion
 import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.db.migrateWithCallbacks
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -10,11 +12,39 @@ class MigrationsExecutor : KoinComponent {
     private val driver: SqlDriver by inject()
 
     fun migrate() {
-        RoarDatabase.Schema.migrate(
-            driver = driver,
-            oldVersion = 2,
-            newVersion = RoarDatabase.Schema.version,
-        )
+            try {
+                RoarDatabase.Schema.migrateWithCallbacks(
+                    driver = driver,
+                    oldVersion = 0,
+                    newVersion = RoarDatabase.Schema.version,
+                    AfterVersion(2) {
+                        driver.execute(
+                            identifier = null,
+                            sql = "ALTER TABLE InteractionEntity ADD COLUMN remindConfig TEXT DEFAULT NULL;",
+                            parameters = 0
+                        )
+                    },
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
     }
+
+    private val migrationTasks = listOf(
+        {
+            RoarDatabase.Schema.migrateWithCallbacks(
+                driver = driver,
+                oldVersion = 0,
+                newVersion = RoarDatabase.Schema.version,
+                AfterVersion(0) {
+                    driver.execute(
+                        identifier = null,
+                        sql = "ALTER TABLE ReminderEntity ADD COLUMN notificationJobId TEXT;",
+                        parameters = 0
+                    )
+                },
+            )
+        }
+    )
 
 }
