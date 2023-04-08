c37a2e0b05ea9bd0e4691bee80bcc7755820c273
artem.zasypalov <gaoyundexinmen@gmail.com>
2023-04-08 12:24:58 +0200

    version bump

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gaoyun.roar.android"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         multiDexEnabled = true
     }
     buildFeatures {
