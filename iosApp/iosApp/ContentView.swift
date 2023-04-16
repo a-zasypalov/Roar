ec4f948c9ad1be97c9a8bb5e2e6135a1fb998d46
artem.zasypalov <gaoyundexinmen@gmail.com>
2023-04-16 14:51:17 +0200

    Rollback Compose iOS

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,14 +1,6 @@
 import SwiftUI
 import sharedLib
 
-//struct ContentView: UIViewControllerRepresentable {
-//    func makeUIViewController(context: Context) -> UIViewController {
-//        Main_iosKt.MainViewController()
-//    }
-//
-//    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
-//}
-
 struct ContentView: View {
     let vm = HomeScreenViewModel()
     
@@ -24,6 +16,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
-            .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
 	}
 }
