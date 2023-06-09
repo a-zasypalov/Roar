0cfc9f6166c2ff062f65afe8f29afb023d77a5a7
artem.zasypalov <gaoyundexinmen@gmail.com>
2023-06-09 21:51:27 +0200

    Add legal pages to Register page

--- androidApp/feature-user-registration/src/main/java/com/gaoyun/feature_user_registration/UserRegistrationForm.kt
+++ androidApp/feature-user-registration/src/main/java/com/gaoyun/feature_user_registration/UserRegistrationForm.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,6 +35,10 @@ import com.gaoyun.common.theme.RoarThemePreview
 fun UserRegistrationForm(
     onRegisterClick: (String) -> Unit
 ) {
+    val uriHandler = LocalUriHandler.current
+
+    val termsAndConditionsUrl = stringResource(id = R.string.url_terms_and_conditions)
+    val privacyPolicyUrl = stringResource(id = R.string.url_privacy_policy)
     var nameState by rememberSaveable { mutableStateOf("") }
 
     Box(
@@ -66,11 +72,37 @@ fun UserRegistrationForm(
             )
         }
 
-        PrimaryElevatedButton(
-            text = stringResource(id = R.string.register_or_login),
-            onClick = { onRegisterClick(nameState) },
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
+        Column {
+            PrimaryElevatedButton(
+                text = stringResource(id = R.string.register_or_login),
+                onClick = { onRegisterClick(nameState) },
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            TextButton(
+                onClick = { uriHandler.openUri(termsAndConditionsUrl) },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.terms_and_conditions_button),
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier.padding(horizontal = 32.dp)
+                )
+            }
+
+            TextButton(
+                onClick = { uriHandler.openUri(privacyPolicyUrl) },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.privacy_policy),
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier.padding(horizontal = 32.dp)
+                )
+            }
+
+            com.gaoyun.common.composables.Spacer(size = 8.dp)
+        }
     }
 }
 
