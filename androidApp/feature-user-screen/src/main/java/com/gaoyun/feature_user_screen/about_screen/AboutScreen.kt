43937f75808e9b6889f944141152c243f97c7053
artem.zasypalov <gaoyundexinmen@gmail.com>
2023-06-09 21:05:52 +0200

    Add social links

--- androidApp/feature-user-screen/src/main/java/com/gaoyun/feature_user_screen/about_screen/AboutScreen.kt
+++ androidApp/feature-user-screen/src/main/java/com/gaoyun/feature_user_screen/about_screen/AboutScreen.kt
@@ -1,5 +1,8 @@
 package com.gaoyun.feature_user_screen.about_screen
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -7,7 +10,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Language
 import androidx.compose.material.icons.filled.Mail
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +25,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -61,10 +72,13 @@ fun AboutScreenDestination(
 
 @Composable
 fun AboutScreenContent() {
+    val scrollState = rememberScrollState()
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
+            .verticalScroll(scrollState)
             .padding(
                 start = 16.dp,
                 end = 16.dp,
@@ -101,58 +115,68 @@ fun AboutScreenContent() {
 
         Spacer(size = 32.dp)
 
-        Row {
-            Surface(
-                tonalElevation = 16.dp,
-                modifier = Modifier
-                    .clip(MaterialTheme.shapes.medium)
-                    .clickable { }
-            ) {
-                Icon(
-                    painterResource(
-                        id = R.drawable.ic_telegram
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(48.dp)
-                        .padding(8.dp)
-                )
-            }
+        LinksBlock()
+    }
+}
 
-            Spacer(size = 24.dp)
+@Composable
+private fun LinksBlock() {
+    val context = LocalContext.current
+    val uriHandler = LocalUriHandler.current
 
-            Surface(
-                tonalElevation = 16.dp,
-                modifier = Modifier
-                    .clip(MaterialTheme.shapes.medium)
-                    .clickable { }
-            ) {
-                Icon(
-                    Icons.Default.Mail,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(48.dp)
-                        .padding(8.dp)
-                )
-            }
+    val telegramUrl = stringResource(id = R.string.url_telegram)
+    val twitterUrl = stringResource(id = R.string.url_twitter)
+    val webUrl = stringResource(id = R.string.url_web)
 
-            Spacer(size = 24.dp)
+    val email = stringResource(id = R.string.url_email)
+    val subject = stringResource(id = R.string.app_name)
 
-            Surface(
-                tonalElevation = 16.dp,
-                modifier = Modifier
-                    .clip(MaterialTheme.shapes.medium)
-                    .clickable { }
-            ) {
-                Icon(
-                    painterResource(id = R.drawable.ic_twitter),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(48.dp)
-                        .padding(8.dp)
-                )
-            }
-        }
+    Row {
+        LinkItem(painterResource(id = R.drawable.ic_telegram), Modifier.clickable { uriHandler.openUri(telegramUrl) })
+        Spacer(size = 24.dp)
+        LinkItem(Icons.Default.Mail, Modifier.clickable { context.sendMail(to = email, subject = subject) })
+        Spacer(size = 24.dp)
+        LinkItem(painterResource(id = R.drawable.ic_twitter), Modifier.clickable { uriHandler.openUri(twitterUrl) })
+        Spacer(size = 24.dp)
+        LinkItem(Icons.Default.Language, Modifier.clickable { uriHandler.openUri(webUrl) })
+    }
+}
+
+@Composable
+private fun LinkItem(icon: ImageVector, modifier: Modifier = Modifier) {
+    LinkItem(
+        icon = rememberVectorPainter(image = icon),
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun LinkItem(icon: Painter, modifier: Modifier = Modifier) {
+    Surface(
+        tonalElevation = 16.dp,
+        modifier = modifier.clip(MaterialTheme.shapes.medium)
+    ) {
+        Icon(
+            icon,
+            contentDescription = null,
+            modifier = Modifier
+                .size(48.dp)
+                .padding(8.dp)
+        )
+    }
+}
+
+fun Context.sendMail(to: String, subject: String) {
+    try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(to))
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        // TODO: Handle case where no email app is available
+    } catch (t: Throwable) {
+        // TODO: Handle potential other type of exceptions
     }
 }
 
