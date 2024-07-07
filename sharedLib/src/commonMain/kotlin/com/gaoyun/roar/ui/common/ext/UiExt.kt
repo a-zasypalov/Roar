e1a593e2e1b5ece648a2c11251cff3161d250e79
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-07-07 19:42:39 +0200

    Cleanup

--- sharedLib/src/commonMain/kotlin/com/gaoyun/roar/ui/common/ext/UiExt.kt
+++ sharedLib/src/commonMain/kotlin/com/gaoyun/roar/ui/common/ext/UiExt.kt
@@ -2,9 +2,7 @@ package com.gaoyun.roar.ui.common.ext
 
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.InternalResourceApi
-import org.jetbrains.compose.resources.LanguageQualifier
 import org.jetbrains.compose.resources.ResourceItem
-import org.jetbrains.compose.resources.StringResource
 
 @OptIn(InternalResourceApi::class)
 internal fun getDrawableByName(idRes: String): DrawableResource {
@@ -18,15 +16,3 @@ internal fun getDrawableByName(idRes: String): DrawableResource {
         )
     )
 }
-
-//@OptIn(InternalResourceApi::class)
-//internal fun getStringByName(idRes: String): StringResource {
-//    return StringResource(
-//        "string:$idRes", idRes,
-//        setOf(
-//            ResourceItem(setOf(LanguageQualifier("de")), "composeResources/roar.sharedlib.generated.resources/values-de/strings.xml", -1, -1),
-//            ResourceItem(setOf(LanguageQualifier("ru")), "composeResources/roar.sharedlib.generated.resources/values-ru/strings.xml", -1, -1),
-//            ResourceItem(setOf(), "composeResources/roar.sharedlib.generated.resources/values/strings.xml", -1, -1),
-//        )
-//    )
-//}
