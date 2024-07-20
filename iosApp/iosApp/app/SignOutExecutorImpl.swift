d981ea2737741eed9a828aabea374d4a69a28e4d
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-07-20 13:28:57 +0200

    Remove all pending notifications on logout

--- iosApp/iosApp/app/SignOutExecutorImpl.swift
+++ iosApp/iosApp/app/SignOutExecutorImpl.swift
@@ -1,9 +1,11 @@
 import sharedLib
 import FirebaseAuth
+import UserNotifications
 
 class SignOutExecutorImpl: SignOutExecutor {
     func signOut() {
         do {
+            UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
             try Auth.auth().signOut()
         } catch {
             print(error.localizedDescription)
