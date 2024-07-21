d6a159c8eedf651261eceaaf0644eab7f5beb34e
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-07-21 23:34:07 +0200

    Fix theme and icons setting for the first sessions (Android)

--- sharedLib/src/androidMain/kotlin/com/gaoyun/roar/util/ThemeChangerAndroid.kt
+++ sharedLib/src/androidMain/kotlin/com/gaoyun/roar/util/ThemeChangerAndroid.kt
@@ -1,5 +1,6 @@
 package com.gaoyun.roar.util
 
+import android.app.Activity
 import android.content.ComponentName
 import android.content.pm.PackageManager
 import org.koin.core.component.KoinComponent
@@ -8,26 +9,30 @@ import org.koin.core.component.KoinComponent
 class ThemeChangerAndroid(private val activityProvider: ActivityProvider) : KoinComponent, ThemeChanger {
     override fun applyTheme() {
         activityProvider.currentActivity?.recreate()
+        activityProvider.mainActivity?.recreate()
     }
 
     override fun activateIcon(icon: AppIcon) {
-        activityProvider.currentActivity?.let {
-            val enable = when (icon) {
-                AppIcon.Roar -> "com.gaoyun.roar.android.ROAR.AMBER"
-                AppIcon.Paw -> "com.gaoyun.roar.android.PAW.AMBER"
-            }
-            val disable = when (icon) {
-                AppIcon.Paw -> "com.gaoyun.roar.android.ROAR.AMBER"
-                AppIcon.Roar -> "com.gaoyun.roar.android.PAW.AMBER"
-            }
-            it.packageManager.setComponentEnabledSetting(
-                ComponentName(it, disable),
-                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
-            )
-            it.packageManager.setComponentEnabledSetting(
-                ComponentName(it, enable),
-                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
-            )
+        activityProvider.currentActivity?.let { tryActivateIcon(it, icon) }
+        activityProvider.mainActivity?.let { tryActivateIcon(it, icon) }
+    }
+
+    private fun tryActivateIcon(activity: Activity, icon: AppIcon) {
+        val enable = when (icon) {
+            AppIcon.Roar -> "com.gaoyun.roar.android.ROAR.AMBER"
+            AppIcon.Paw -> "com.gaoyun.roar.android.PAW.AMBER"
         }
+        val disable = when (icon) {
+            AppIcon.Paw -> "com.gaoyun.roar.android.ROAR.AMBER"
+            AppIcon.Roar -> "com.gaoyun.roar.android.PAW.AMBER"
+        }
+        activity.packageManager.setComponentEnabledSetting(
+            ComponentName(activity, disable),
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+        )
+        activity.packageManager.setComponentEnabledSetting(
+            ComponentName(activity, enable),
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+        )
     }
 }
