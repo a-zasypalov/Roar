bb5b21e18a4afaf998372f794f8215f85ac3c96e
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-07-21 18:59:06 +0200

    Add debug print to iOS project

---
+++ iosApp/iosApp/app/DebugPrint.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+    Swift.print(items, separator: separator, terminator: terminator)
+    #endif
+}
